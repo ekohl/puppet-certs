adec2d6dfb74a86bf76dfdd2f49d607f895817be
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-07-21 19:29:31 +0200

    [WIP] Demonstrate that /etc/foreman isn't needed
    https://github.com/theforeman/puppet-katello_devel/pull/264#discussion_r672465771
    led to this. We had a discussion if we needed /etc/foreman. This shows
    it doesn't. This isn't really a good test since the old code should
    still be tested, but it's a POC.

--- spec/acceptance/foreman_spec.rb
+++ spec/acceptance/foreman_spec.rb
@@ -3,26 +3,22 @@ require 'spec_helper_acceptance'
 describe 'certs::foreman' do
   fqdn = fact('fqdn')
 
-  before(:all) do
-    manifest = <<~MANIFEST
-      file { '/etc/foreman':
-        ensure => directory,
-      }
-
-      group { 'foreman':
-        ensure => present,
-        system => true,
-      }
-    MANIFEST
-    apply_manifest(manifest, catch_failures: true)
-  end
-
   context 'with default parameters' do
     it_behaves_like 'an idempotent resource' do
-      let(:manifest) { 'include certs::foreman' }
+      let(:manifest) do
+       <<~MANIFEST
+         class { 'certs::foreman':
+           client_cert           => '/tmp/foreman_client_cert.pem',
+           client_key            => '/tmp/foreman_client_key.pem',
+           ssl_ca_cert           => '/tmp/foreman_proxy_ca.pem',
+           owner                 => 'root',
+           group                 => 'root',
+         }
+        MANIFEST
+      end
     end
 
-    describe x509_certificate('/etc/foreman/client_cert.pem') do
+    describe x509_certificate('/tmp/foreman_client_cert.pem') do
       it { should be_certificate }
       it { should be_valid }
       it { should have_purpose 'client' }
@@ -31,27 +27,27 @@ describe 'certs::foreman' do
       its(:keylength) { should be >= 4096 }
     end
 
-    describe file('/etc/foreman/client_cert.pem') do
+    describe file('/tmp/foreman_client_cert.pem') do
       it { should be_file }
       it { should be_mode 440 }
       it { should be_owned_by 'root' }
-      it { should be_grouped_into 'foreman' }
+      it { should be_grouped_into 'root' }
     end
 
-    describe x509_private_key('/etc/foreman/client_key.pem') do
+    describe x509_private_key('/tmp/foreman_client_key.pem') do
       it { should_not be_encrypted }
       it { should be_valid }
-      it { should have_matching_certificate('/etc/foreman/client_cert.pem') }
+      it { should have_matching_certificate('/tmp/foreman_client_cert.pem') }
     end
 
-    describe file('/etc/foreman/client_key.pem') do
+    describe file('/tmp/foreman_client_key.pem') do
       it { should be_file }
       it { should be_mode 440 }
       it { should be_owned_by 'root' }
-      it { should be_grouped_into 'foreman' }
+      it { should be_grouped_into 'root' }
     end
 
-    describe x509_certificate('/etc/foreman/proxy_ca.pem') do
+    describe x509_certificate('/tmp/foreman_proxy_ca.pem') do
       it { should be_certificate }
       it { should be_valid }
       its(:issuer) { should eq("C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fqdn}") }
